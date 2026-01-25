87a3b3752427a89a3c67cdefe49029ba4dd18eeb
kup-kup <konka.um@gmail.com>
2026-01-25 14:28:30 +0700

    added time_perf

--- lib.py
+++ lib.py
@@ -2,50 +2,62 @@ import os
 import json
 import pandas as pd
 import sys
+from time import perf_counter
+
+class time_perf():
+    def __init__(self, name=None):
+        self.name = name
+        self.s = 0
+    
+    def __enter__(self):
+        self.s = perf_counter()
+    
+    def __exit__(self, *_):
+        if self.name:
+            print(self.name, end=': ')
+        print(perf_counter()-self.s)
 
 class Gatherer:
     def __init__(self):
         self.root = os.getcwd()
 
         # check for root/data directory
-        self.data_path = os.path.join(self.root, 'temp')
+        self.data_path = os.path.join(self.root, 'data')
         if not os.path.exists(self.data_path):
             raise FileNotFoundError(f"'data' directory not found in {self.root}")
     
-    def _iterate(self, action, verbose = False) -> pd.DataFrame:
-        res = pd.DataFrame()
+    def _iterate(self):
+        """main method to iterate through all conversations folders"""
         for entry in os.scandir(self.data_path):
-            if (
-                not entry.is_dir()
-                or len(entry.name) != 36
-            ):
+            if not entry.is_dir() or len(entry.name) != 36:
                 continue
-
-            if verbose:            
-                print(f"Processing directory: {entry.name}")
-
-            try:
-                if action:
-                    res = pd.concat([res, action(entry)], ignore_index=True)
-            except Exception as e:
-                print(f"Error processing {entry.name}: {e}")
-                
-        return res
+            yield entry
 
     def get_metadata_df(self, verbose) -> pd.DataFrame:
-
-        def _action(entry):
+        """get metadata, return pd.DataFrame"""
+        res = pd.DataFrame()
+        for entry in self._iterate():
             metadata_path = os.path.join(entry.path, 'metadata.json')
-            if os.path.exists(metadata_path):
-                with open(metadata_path, 'r') as f:
-                    metadata = json.load(f)
-                return pd.DataFrame([metadata])
-            return pd.DataFrame()
+
+            # skip if no metadata
+            if not os.path.exists(metadata_path):
+                if verbose:
+                    print(f"Metadata not found for: {entry.name}")
+                continue
+
+            # read metadata
+            with open(metadata_path, 'r') as f:
+                metadata = json.load(f)
+            entry_df = pd.DataFrame([metadata])
+            res = pd.concat([res, entry_df], ignore_index=True)
+
+            if verbose:
+                print(f"Processed: {entry.name}")
     
-        return self._iterate(_action, verbose)
-        
+        return res
 
 if __name__ == "__main__":
     gatherer = Gatherer()
-    data = gatherer.get_metadata_df(verbose=True)
+    with time_perf("Metadata Gathering"): # ~ 1.5s
+        data = gatherer.get_metadata_df(verbose=False)
     print(data.info())
